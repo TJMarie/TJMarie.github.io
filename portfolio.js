c862d8d56ed820b20840187179933a78ce72cdba
Adrien Jones <tjones69@memphis.edu>
2024-04-09 14:14:50 -0500

    Implemented filter function for viewing paintings using checkboxes

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,7 @@ var slideIndex = 0
 carousel()
 
 function carousel() {
+    // Cycles through paintings every 2 seconds
     var i
     var x = document.getElementsByClassName("gallery")
     for (i = 0; i < x.length; i++) {
@@ -11,4 +12,115 @@ function carousel() {
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex - 1].style.display = "block"
     setTimeout(carousel, 2000)
+}
+
+var itemChecked = false
+var abstractChecked = false
+var landscapeChecked = false
+var peopleChecked = false
+var skullChecked = false
+var coverChecked = false
+
+function filterPaintings(c) {
+    console.log(c)
+    // toggle checked
+    if (c == "item" && itemChecked) { 
+        itemChecked = false
+    } else if (c == "abstract" && abstractChecked) {
+        abstractChecked = false
+    } else if (c == "landscape" && landscapeChecked) {
+        landscapeChecked = false
+    } else if (c == "people" && peopleChecked) {
+        peopleChecked = false
+    } else if (c == "skull" && skullChecked) {
+        skullChecked = false
+    } else if (c == "cover" && coverChecked) {
+        coverChecked = false
+    } else if (c == "item" && !itemChecked) {
+        itemChecked = true
+    } else if (c == "abstract" && !abstractChecked) {
+        abstractChecked = true
+    } else if (c == "landscape" && !landscapeChecked) {
+        landscapeChecked = true
+    } else if (c == "people" && !peopleChecked) {
+        peopleChecked = true
+    } else if (c == "skull" && !skullChecked) {
+        skullChecked = true
+    } else if (c == "cover" && !coverChecked) {
+        coverChecked = true
+    }
+    // hide unchecked items first
+    if (!itemChecked) {
+        var elements = Array.from(document.getElementsByClassName("item"))
+        hideVisibility(elements)
+    }
+    if (!abstractChecked) {
+        var elements = Array.from(document.getElementsByClassName("abstract"))
+        hideVisibility(elements)
+    }
+    if (!landscapeChecked) {
+        var elements = Array.from(document.getElementsByClassName("landscape"))
+        hideVisibility(elements)
+    } 
+    if (!peopleChecked) {
+        var elements = Array.from(document.getElementsByClassName("people"))
+        hideVisibility(elements)
+    } 
+    if (!skullChecked) {
+        var elements = Array.from(document.getElementsByClassName("skull"))
+        hideVisibility(elements)
+    } 
+    if (!coverChecked) {
+        var elements = Array.from(document.getElementsByClassName("cover"))
+        hideVisibility(elements)
+    } 
+    // show checked elements
+    if (itemChecked) {
+        var elements = Array.from(document.getElementsByClassName("item"))
+        showVisibility(elements)
+    } 
+    if (abstractChecked) {
+        var elements = Array.from(document.getElementsByClassName("abstract"))
+        showVisibility(elements)
+    } 
+    if (landscapeChecked) {
+        var elements = Array.from(document.getElementsByClassName("landscape"))
+        showVisibility(elements)
+    } 
+    if (peopleChecked) {
+        var elements = Array.from(document.getElementsByClassName("people"))
+        showVisibility(elements)
+    } 
+    if (skullChecked) {
+        var elements = Array.from(document.getElementsByClassName("skull"))
+        showVisibility(elements)
+    } 
+    if (coverChecked) {
+        var elements = Array.from(document.getElementsByClassName("cover"))
+        showVisibility(elements)
+    }
+}
+
+function toggleVisibility(elements) {
+    elements.forEach(el => {
+        visible = el.checkVisibility()
+        if (visible) {
+            el.style.display = 'none'
+        } else {
+            el.style.display = "inline-grid"
+        }
+        
+    });
+
+}
+function hideVisibility(elements) {
+    elements.forEach(el => {
+        el.style.display = 'none'
+    })
+}
+
+function showVisibility(elements) {
+    elements.forEach(el => {
+        el.style.display = 'inline-grid'
+    })
 }
